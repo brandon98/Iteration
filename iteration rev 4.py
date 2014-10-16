6b56b7fe43ee7552ac7665e280cb4a03568482fd
brandon98 <40203@longroad.ac.uk>
2014-10-16 20:26:40 +0100

    iteration revision exercise 4

---
+++ iteration rev 4.py
@@ -0,0 +1,12 @@
+#Brandon Dickson
+#Iteration revision exercise 4
+#15-10-14
+
+number = 0
+
+
+while number not in range(10,21):
+    number=int(input("Please enter a number between 10 and 20:"))
+
+print(number)
+   
