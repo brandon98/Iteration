f116b8bc09d1ce0574c57bbc25070147bf78763a
brandon98 <40203@longroad.ac.uk>
2014-10-20 18:16:06 +0100

    development ex 2

---
+++ development ex 2.py
@@ -0,0 +1,9 @@
+#Brandon Dickson
+#Development exercise 2
+#19-10-14
+
+stars=int(input("Please enter amount of stars to be printed:"))
+times=int(input("Please enter amount of times it should be repeated:"))
+
+for counter in range(times):
+    print(stars*"*")
