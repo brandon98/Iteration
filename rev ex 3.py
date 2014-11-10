663a8ac062cd8aa22be8fdb31a183edaa49aa275
brandon98 <40203@longroad.ac.uk>
2014-11-10 20:49:01 +0000

    rev ex 3

--- rev ex 3.py
+++ rev ex 3.py
@@ -2,15 +2,15 @@
 #Revision ex 3
 #3-11-2014
 count=0
-
+total=0
 amount=int(input("Please enter amount of numbers to be averaged:"))
 
 while count < amount:
     number=int(input("Please enter number:"))
-    number=number+number
+    total=total+number
     count=count+1
     
-count=count+1
-average= number/amount
+
+average= total/amount
 
 print("Average:", average)
